6b0456a1e3ba641868ddec6ad476dc26284054bf
zaidshado777-droid <zaidshado777@gmail.com>
2026-08-24 23:41:56 +1000

    Update binding address and password in config

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const enableWorkers = os.cpus().length !== 1;
 module.exports = {
     //// HOSTING CONFIGURATION ////
 
-    bindingAddress: '127.0.0.1',
+    bindingAddress: '0.0.0.0',
     port: 8080,
     crossDomainPort: 8081,
     publicDir: path.join(__dirname, '../public'), // set to null to disable
@@ -32,7 +32,7 @@ module.exports = {
     // },
 
     // enforce a password for creating new sessions. set to null to disable
-    password: 'sharkie4life',
+    password: 'variabledash',
 
     // disable or enable localStorage sync (turn off if clients send over huge localStorage data, resulting in huge memory usages)
     disableLocalStorageSync: false,

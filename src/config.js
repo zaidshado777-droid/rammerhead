651e04ece8d8a760936def851fe8011a56699339
zaidshado777-droid <zaidshado777@gmail.com>
2026-08-25 00:10:03 +1000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,18 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
     bindingAddress: '0.0.0.0',
     port: process.env.PORT || 8080,
     crossPort: 8081,
-    publicLogs: true,
-    password: '',
-    workers: 1,
     ssl: {
         enabled: false,
         key: '',
         cert: ''
     },
+    crossServerKey: 'shh-its-a-secret',
+    password: '',
+    workers: 1,
+    reverseProxy: false,
+    publicLogs: true,
+    loggingLevel: 'info',
     diskCache: {
         enabled: false,
         maxAge: 86400000,
         path: './cache'
+    },
+    generatePrefix: (type) => {
+        const time = new Date().toISOString();
+        return `(${process.pid}) [${time}] [${type.toUpperCase()}] `;
+    },
+    getIP: (req) => {
+        return req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     }
 };
